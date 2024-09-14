28c51d8288182e859923b67fda886a54fd06aef6
Greg Dev <admin@mail.cuttlenet.com>
2024-09-13 22:28:24 -0400

    add storybook

---
+++ src/pages/api/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type HomeResponse = {
+    hello: string
+}
+
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
+
+}
+
+export default handler;
