9566bff1083f88f45ccb3049621f4cacd422941c
Greg Dev <admin@mail.cuttlenet.com>
2024-09-13 22:58:45 -0400

    add api handler

--- src/pages/api/index.ts
+++ src/pages/api/index.ts
@@ -1,11 +1,24 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 type HomeResponse = {
-    hello: string
+    data: {
+        hello: string
+        error?: string
+    }
 }
 
-const handler = (req: NextApiRequest, res: NextApiResponse) => {
+type ErrorResponse = {
+    data: {
+        error: 'goodbye world'
+    }
+}
 
+const handler = (req: NextApiRequest, res: NextApiResponse<HomeResponse | ErrorResponse>) => {
+    if (req.method === "GET") {
+        return res.status(200).json({ data: {hello: 'world' }} );
+    }
+    res.status(402)
+    return res.send({ data : { error: 'goodbye world' } });
 }
 
 export default handler;
