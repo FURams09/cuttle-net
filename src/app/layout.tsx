5b2fb6d15aa8d64eb3acc220f310af7aea00507e
Greg Dev <admin@mail.cuttlenet.com>
2024-10-03 18:03:31 -0400

    fix style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,7 @@ export default function RootLayout({
                     fbq('track', 'PageView');`}}>
                     
                     </script>
-                    <noscript><img height="1" width="1" style="display:none"
+                    <noscript><img height="1" width="1" style={{ display: 'none' }}
                     src="https://www.facebook.com/tr?id=1276073900410008&ev=PageView&noscript=1"
                     /></noscript>
 {/* <!-- End Meta Pixel Code --> */}
